89eec4c83548d43f5191116c6bc6769e4ca63aa0
Barnabé Samel <barnabe.samel@gmail.com>
2022-10-09 11:27:18 +0200

    finish building function deleteProduct (works)

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,17 @@
 const cartItems = document.querySelector("#cart__items");
+var index;
+
+/* ===============================
+Get the cart from the localStorage
+================================ */
+
+function getCart() {
+    let cart = [];
+    if (localStorage.getItem("cartStorage") != null) {
+        cart = JSON.parse(localStorage.getItem("cartStorage"));
+    }
+    return cart;
+}
 
 /* ==========================================
 Display the items in localStorage on the page
@@ -7,7 +20,7 @@ function displayCart() {
     
     fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((product) => {
+    .then(() => {
 
         let cart = JSON.parse(localStorage.getItem("cartStorage"));
         //For each product in the cart, display according to the html model.
@@ -47,7 +60,7 @@ function changeProductQuantity() {
 
     fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((product) => {
+    .then(() => {
 
         const allItems = document.querySelectorAll(`.cart__item`);
         let cart = JSON.parse(localStorage.getItem("cartStorage"));
@@ -56,20 +69,69 @@ function changeProductQuantity() {
         //For each item in cart Page, get the right input and add it a 'change' event
         .forEach ( item => {
             itemQuantity = item.querySelector('input[name = "itemQuantity"]');
-            itemQuantity.addEventListener("change", (event) => {
-                const article = event.target.closest(".cart__item");
+            itemQuantity.addEventListener("change", (product) => {
+                const article = product.target.closest(".cart__item");
                 // For each product in cartStorage, if the targeted product is the same item as the one displayed on the page, then if we change its quantity, it's updated in the cartStorage
                 cart.forEach(storedProduct => {
                     if(storedProduct.id == article.dataset.id && storedProduct.color == article.dataset.color){
-                        storedProduct.quantity = event.target.value;
+                        storedProduct.quantity = product.target.value;
                     }
-                });
+                })
                 localStorage.setItem("cartStorage",JSON.stringify(cart));        
             })
         })
     })
     .catch((err) => console.log(err));
 }
+//===================================================================================
+
+/* =====================
+Delete article from cart
+====================== */
+function deleteProduct () {
+    fetch("http://localhost:3000/api/products")
+        .then((res) => res.json())
+        .then(() => {
+
+            const allItems = document.querySelectorAll(`.cart__item`);
+
+            Array.from(allItems) //Node list => array conversion
+            //For each item in cart Page, get the right input and add it a 'change' event
+            .forEach ( item => {
+                deleteBtn = item.querySelector('.deleteItem');
+                deleteBtn.addEventListener("click", (product) => {
+                    const article = product.target.closest(".cart__item");
+                    // For each product in cartStorage, if the targeted product is the same item as the one displayed on the page, then if we change its quantity, it's updated in the cartStorage
+                    cart = getCart();
+                    cart.forEach(storedProduct => {
+                        if(storedProduct.id == article.dataset.id && storedProduct.color == article.dataset.color){
+                            index =  cart.indexOf(storedProduct);
+                            cart.splice(index,1)
+                            localStorage.setItem("cartStorage",JSON.stringify(cart));
+                            window.location.reload();
+                        }  
+                    })
+                })
+            })
+        })
+        .catch((err) => console.log(err));
+}
+
 
 displayCart()
 changeProductQuantity()
+deleteProduct () 
+
+
+
+
+
+
+// for (let i = 0; i < cart.length; i++) {
+//     console.log(cart[i].id);
+//     if(cart[i].id == article.dataset.id && cart[i].color == article.dataset.color){
+//         cart.splice(i,1)
+//         localStorage.setItem("cartStorage",JSON.stringify(cart));
+//         window.location.reload();
+//     }
+// }
